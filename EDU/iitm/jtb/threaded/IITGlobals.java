296780e5612181655d514c60ae5ddcae2da3f5f9
paulpell <mail.paulpellegrini@gmail.com>
2012-06-11 06:51:57 +0200

    added some globals

--- EDU/iitm/jtb/threaded/IITGlobals.java
+++ EDU/iitm/jtb/threaded/IITGlobals.java
@@ -1,5 +1,7 @@
 package EDU.iitm.jtb.threaded;
 
 public class IITGlobals {
-	public final static String threadedVisitorName = "ThreadedVisitor";
+	public final static String threadedDFVisitorName = "ThreadedVisitor";
+	public final static String progName = "JTB-parallel";
+	public final static String version = "1.0b";
 }
