1fbbff739c8f01c9c7d24ece53f50c5d6f10b61e
paulpell <mail.paulpellegrini@gmail.com>
2012-06-25 13:22:40 +0200

    added examples of visitor creation

--- Main.java
+++ Main.java
@@ -15,11 +15,24 @@ public class Main {
 						System.exit(0);
 				}
 
-				JTBParser p = new JTBParser(in);
+				JTBParser p = new JTBParser(in);// for JTB grammar
+                //JavaParser p =new JavaParser(in); // for Java.jj
 				try {
 					Node r = p.CompilationUnit();
-					ThreadedVisitor v = new ThreadedVisitor();
-					r.accept(v, true);
+
+          // uncomment one of the following visitors to test
+
+                    //GJThreadedVisitor v = new GJThreadedVisitor();
+                    //r.accept(v, null, true);
+
+					//GJNoArguThreadedVisitor va = new GJNoArguThreadedVisitor();
+                    //r.accept(va, true);
+                    
+                    //GJVoidThreadedVisitor vv = new GJVoidThreadedVisitor();
+					//r.accept(v, null, true);
+
+                    //ThreadedVisitor v2 = new ThreadedVisitor();
+                    //r.accept(v2, true);
 					
 					//r.accept(new DepthFirstVisitor());
 				} catch (ParseException e) {
